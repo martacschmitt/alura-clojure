6f1fd2433aa21ae58732050266bce39aecea9a73
Marta Schmitt <marta.schmitt@marta.schmitt>
2022-05-04 19:14:03 -0300

    Vetores, listas, conjuntos e filas

--- hospital/src/hospital/core.clj
+++ hospital/src/hospital/core.clj
@@ -1,3 +1,10 @@
-(ns hospital.core)
+(ns hospital.core
+  (:use [clojure pprint])
+  (:require [hospital.model :as h.model]))
+
+(let [hospital-do-gui (h.model/novo-hospital)]
+  (pprint hospital-do-gui))
+
+(pprint h.model/fila-vazia)
 
 
