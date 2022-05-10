8a908dd17d04497ef7ce04335fb126cb429263fa
Marta Schmitt <marta.schmitt@marta.schmitt>
2022-05-10 16:58:47 -0300

    Testes com schemas

--- clojure-testes/hospital/src/hospital/model.clj
+++ clojure-testes/hospital/src/hospital/model.clj
@@ -1,4 +1,5 @@
-(ns hospital.model)
+(ns hospital.model
+  (:require [schema.core :as s]))
 
 (def fila-vazia clojure.lang.PersistentQueue/EMPTY)
 
@@ -8,3 +9,12 @@
    :laboratorio1 fila-vazia
    :laboratorio2 fila-vazia
    :laboratorio3 fila-vazia})
+
+(s/def PacienteID s/Str)
+(s/def Departamento (s/queue PacienteID))
+(s/def Hospital {s/Keyword Departamento})
+
+;(s/validate PacienteID "Guilherme")
+;(s/validate PacienteID 15)
+;(s/validate Departamento (conj fila-vazia "Guilherme" "Daniela"))
+;(s/validate Hospital {:espera (conj fila-vazia "Guilherme" "Daniela")})
