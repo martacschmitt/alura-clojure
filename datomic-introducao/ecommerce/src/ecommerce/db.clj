865937781f9b4d961df127f479321cc8e166777e
Marta Schmitt <marta.schmitt@marta.schmitt>
2022-05-13 18:08:37 -0300

    Datomic - Schema e transacoes

---
+++ datomic-introducao/ecommerce/src/ecommerce/db.clj
@@ -0,0 +1,43 @@
+(ns ecommerce.db
+  (:use [clojure.pprint])
+  (:require [datomic.api :as d]))
+
+(def db-uri "datomic:dev://localhost:4334/ecommerce")
+
+(defn abre-conexao []
+  (d/create-database db-uri)
+  (d/connect db-uri))
+
+(defn apaga-banco []
+  (d/delete-database db-uri))
+
+
+; Produtos
+; id?
+; nome String 1 ==> Computador Novo
+; slug String 1 ==> /computador_novo
+; preco ponto flutuante 1 ==> 3500.10
+
+; id_entidade atributo valor
+; 15 :produto/nome Computador Novo
+; 15 :produto/slug /computador_novo
+; 15 :produto/preco 3500.10
+; 17 :produto/nome Telefone Caro
+; 17 :produto/slug /telefone
+; 17 :produto/preco 8888.88
+
+(def schema [{:db/ident       :produto/nome
+              :db/valueType   :db.type/string
+              :db/cardinality :db.cardinality/one
+              :db/doc         "O nome de um produto"}
+             {:db/ident       :produto/slug
+              :db/valueType   :db.type/string
+              :db/cardinality :db.cardinality/one
+              :db/doc         "O caminho para acessar esse produto via hhtp"}
+             {:db/ident       :produto/preco
+              :db/valueType   :db.type/bigdec
+              :db/cardinality :db.cardinality/one
+              :db/doc         "O preço de um produto com precisão monetária"}])
+
+(defn cria-schema [conn]
+  (d/transact conn schema))
